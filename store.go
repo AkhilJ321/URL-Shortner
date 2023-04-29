a06187d8856fca9622fad81808660a73d3260061
AkhilJ321 <jamwalakhil936@gmail.com>
2023-04-29 17:12:56 +0530

    Used Goroutines for Perforamnce

--- store.go
+++ store.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	//	"bufio"
 	"encoding/gob"
 	"io"
 	"log"
@@ -8,49 +9,28 @@ import (
 	"sync"
 )
 
+const saveQueueLength = 1000
+
 type URLStore struct {
 	urls map[string]string
 	mu   sync.RWMutex
-	file *os.File
+	save chan record
 }
+
 type record struct {
 	Key, URL string
 }
 
 func NewURLStore(filename string) *URLStore {
-	s := &URLStore{urls: make(map[string]string)}
-	f, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
-	if err != nil {
-		log.Fatal("URLStore:", err)
+	s := &URLStore{
+		urls: make(map[string]string),
+		save: make(chan record, saveQueueLength),
 	}
-	s.file = f
-	if err := s.load(); err != nil {
-		log.Println("Error loading data in URLStore:", err)
+	if err := s.load(filename); err != nil {
+		log.Println("Error loading URLStore:", err)
 	}
+	go s.saveLoop(filename)
 	return s
-}
-
-func (s *URLStore) save(key, url string) error {
-	e := gob.NewEncoder(s.file)
-	return e.Encode(record{key, url})
-}
-
-func (s *URLStore) load() error {
-	if _, err := s.file.Seek(0, 0); err != nil {
-		return err
-	}
-	d := gob.NewDecoder(s.file)
-	var err error
-	for err == nil {
-		var r record
-		if err = d.Decode(&r); err == nil {
-			s.Set(r.Key, r.URL)
-		}
-	}
-	if err == io.EOF {
-		return nil
-	}
-	return err
 }
 
 func (s *URLStore) Get(key string) string {
@@ -78,12 +58,54 @@ func (s *URLStore) Count() int {
 func (s *URLStore) Put(url string) string {
 	for {
 		key := genKey(s.Count())
-		if s.Set(key, url) {
-			if err := s.save(key, url); err != nil {
-				log.Println("Error saving to URLStore:", err)
-			}
+		if ok := s.Set(key, url); ok {
+			s.save <- record{key, url}
 			return key
 		}
 	}
 	panic("shouldn't get here")
+}
+
+func (s *URLStore) load(filename string) error {
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Println("Error opening URLStore:", err)
+		return err
+	}
+	defer f.Close()
+	// buffered reading:
+	// b := bufio.NewReader(f)
+	// d := gob.NewDecoder(b)
+	d := gob.NewDecoder(f)
+	for err == nil {
+		var r record
+		if err = d.Decode(&r); err == nil {
+			s.Set(r.Key, r.URL)
+		}
+	}
+	if err == io.EOF {
+		return nil
+	}
+	// error occurred:
+	log.Println("Error decoding URLStore:", err) // map hasn't been read correctly
+	return err
+}
+
+func (s *URLStore) saveLoop(filename string) {
+	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatal("Error opening URLStore: ", err)
+	}
+	defer f.Close()
+	e := gob.NewEncoder(f)
+	// buffered encoding:
+	// b := bufio.NewWriter(f)
+	// e := gob.NewEncoder(b)
+	// defer b.Flush()
+	for {
+		r := <-s.save // takes a record from the channel
+		if err := e.Encode(r); err != nil {
+			log.Println("Error saving to URLStore: ", err)
+		}
+	}
 }
